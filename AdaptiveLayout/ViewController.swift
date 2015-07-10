351d00a73212bcb30bcda335c2d3d29a9fd7ad3e
Yung Dai <yungchidai@gmail.com>
2015-07-10 09:44:38 -0400

    Initial Commit

---
+++ AdaptiveLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by Yung Dai on 2015-07-10.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
